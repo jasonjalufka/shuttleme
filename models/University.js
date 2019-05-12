261095f8dbbaecf19ad2696186aa2100d9748312
Jason Jalufka <jasonjalufka@gmail.com>
2019-05-12 14:28:44 -0700

    Add University model

--- models/University.js
+++ models/University.js
@@ -1,6 +1,60 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const statesArray = [
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DC",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY"
+];
+
 // Create Schema
 const UniversitySchema = new Schema({
   name: {
@@ -12,9 +66,31 @@ const UniversitySchema = new Schema({
     required: true,
     unique: true
   },
-  location: {
+  city: {
     type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    uppercase: true,
     required: true,
-    unique: true
+    enum: statesArray
+  },
+  zip: {
+    type: Number,
+    required: true
+  },
+  lat: {
+    type: String,
+    required: true
+  },
+  lon: {
+    type: String,
+    required: true
+  },
+  mascot: {
+    type: String
   }
 });
+
+module.exports = University = mongoose.model("university", UniversitySchema);
