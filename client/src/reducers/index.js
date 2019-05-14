c7b4c58f3cb50cd6a71a88a6ed1fff13fd80b6a3
Jason Jalufka <jasonjalufka@gmail.com>
2019-05-14 14:31:55 -0700

    Remove unnecessary reducers, update combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import authReducer from "./authReducer";
-import preferenceReducer from "./preferenceReducer";
 import geojsonReducer from "./geojsonReducer";
 
 export default combineReducers({
   error: errorReducer,
   auth: authReducer,
-  prefs: preferenceReducer,
   geojson: geojsonReducer
 });
