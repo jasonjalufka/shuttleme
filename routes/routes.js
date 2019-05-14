7e25cd4e0fb6334a11a41f3c7968ea446e18b5b6
Jason Jalufka <jasonjalufka@gmail.com>
2019-05-14 10:32:52 -0700

    Add routes; Add auth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,19 @@
 const University = require("../controllers/university");
 const User = require("../controllers/user");
 const Auth = require("../controllers/auth");
+const Doublemap = require("../controllers/doublemap");
 const auth = require("../middleware/auth");
 
 module.exports = app => {
   app.route("/api/auth").post(Auth.authenticate);
-  app.route("/api/auth/user", auth).get(Auth.getUser);
-  app.route("/api/dashboard");
+  app.route("/api/auth/user").get(auth, Auth.getUser);
+  app.route("/api/dashboard/:universityID");
   app.route("/api/universities").get(University.get);
   app.route("/api/doublemap/buses");
+  app.route("/api/doublemap/buses/:busId");
   app.route("/api/doublemap/stops");
-  app.route("/api/geojson/buses");
-  app.route("/api/geojson/route");
-  app.route("/api/geojson/stops");
+  app.route("/api/geojson/buses/:universityID");
+  app.route("/api/geojson/route/:universityID");
+  app.route("/api/geojson/stops/:universityID");
   app.route("/api/users").post(User.register);
 };
