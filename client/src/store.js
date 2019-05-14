617bbc46ec466a978e2b018ca51ef1ec5444591e
Jason Jalufka <jasonjalufka@gmail.com>
2019-05-14 16:21:38 -0700

    Update store to handle browsers without devTools

--- client/src/store.js
+++ client/src/store.js
@@ -15,8 +15,9 @@ if (window.navigator.userAgent.includes("Chrome")) {
     initialState,
     compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      ...(window.__REDUX_DEVTOOLS_EXTENSION__
+        ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+        : [])
     )
   );
 } else {
