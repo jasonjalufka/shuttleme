63227e6f4832da13eec99e3afb4836ae224e5407
Jason Jalufka <jasonjalufka@gmail.com>
2019-05-14 14:32:46 -0700

    Remove unused routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,15 +6,33 @@ const Geojson = require("../controllers/geojson");
 const auth = require("../middleware/auth");
 
 module.exports = app => {
+  // @route  POST api/auth
+  // @desc   Authenticate a user using email/password
+  // @access Public
   app.route("/api/auth").post(Auth.authenticate);
+  // @route  GET api/auth/user
+  // @desc   Get information of currently logged in user
+  // @access Private
   app.route("/api/auth/user").get(auth, Auth.getUser);
   app.route("/api/dashboard/:universityID");
+  // @route  GET api/universities
+  // @desc   Get all university data
+  // @access Public
   app.route("/api/universities").get(University.get);
-  app.route("/api/doublemap/buses");
-  app.route("/api/doublemap/buses/:busId");
-  app.route("/api/doublemap/stops");
+  // @route  GET api/geojson/buses/:universityID
+  // @desc   Get all bus data for university in geojson format
+  // @access Public
   app.route("/api/geojson/buses/:universityID").get(Geojson.getBuses);
+  // @route  GET api/geojson/routes/:universityID
+  // @desc   Get all route data for university in geojson format
+  // @access Public
   app.route("/api/geojson/routes/:universityID").get(Geojson.getRoutes);
+  // @route  GET api/geojson/stops/:universityID
+  // @desc   Get all active stop data for university in geojson format
+  // @access Public
   app.route("/api/geojson/stops/:universityID").get(Geojson.getStops);
+  // @route  POST api/users
+  // @desc   Register a new user
+  // @access Public
   app.route("/api/users").post(User.register);
 };
